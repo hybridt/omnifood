ce055ed08d70785e0b4176f58bde422796283947
hybridt <hokagemanns@gmail.com>
2024-09-12 21:53:45 +0500

    fix: add scroll toggling when open/close menu

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const burgerBtn = document.querySelector('.burger');
 const header = document.querySelector('.header');
 const navList = document.querySelector('.nav__list');
+const html = document.documentElement;
+
+function toggleMenu(element) {
+  if (screen.width <= 976) {
+    element.classList.toggle('nav-opened');
+    if (element.classList.contains('nav-opened')) {
+      disableScroll();
+    } else {
+      enableScroll();
+    }
+  }
+}
+
+function enableScroll() {
+  html.style.removeProperty('overflow');
+}
+
+function disableScroll() {
+  html.style.overflow = 'hidden';
+}
 
 burgerBtn.addEventListener('click', () => {
-  header.classList.toggle('nav-opened');
+  toggleMenu(header);
 });
 
 navList.addEventListener('click', (e) => {
   const clickedItem = e.target;
-  if (clickedItem.classList.contains('nav__link') || clickedItem.classList.contains('nav__button')) {
-    header.classList.toggle('nav-opened');
+  const isClickedLink = clickedItem.classList.contains('nav__link') || clickedItem.classList.contains('nav__button');
+  if (isClickedLink) {
+    toggleMenu(header);
   }
 });
